c248086b69bffb7e02be17b90d41699ae56344af
Nestorfron <nestorfrones07@hotmail.com>
2025-01-13 18:51:10 -0300

    .env

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST || '127.0.0.1',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    seederStorage: 'sequelize',
   },
 };
+
